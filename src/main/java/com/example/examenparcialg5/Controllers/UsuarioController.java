61021917eed7ea90f4fbc8f3254c2bd206c2fe41
Eric Benites <eric.benites@pucp.edu.pe>
2020-06-08 07:20:26 -0500

    casi crud

--- src/main/java/com/example/examenparcialg5/Controllers/UsuarioController.java
+++ src/main/java/com/example/examenparcialg5/Controllers/UsuarioController.java
@@ -9,15 +9,16 @@ import com.example.examenparcialg5.Repository.ProductoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.jws.WebParam;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 
 
 
@@ -30,17 +31,35 @@ public class UsuarioController {
     UsuarioRepository usuarioRepository;
 
     @GetMapping(value = {"", "/"})
-    public String nuevo (){
+    public String nuevo (@ModelAttribute("usuario") Usuario usuario){
+
 
         return "usuario/nuevo";
     }
 
+    @GetMapping("/gestores")
+    public String listagestores (Model model){
+        int rol_idrol = 2;
+        model.addAttribute("listaGestores", usuarioRepository.obtenerGestores(rol_idrol));
+        return "usuario/lista";
+    }
+
+    //ya que no se puede importar javax.validation.* no se puede agregar el @Valid
+
     @PostMapping("/registrar")
-    public String registrarUsuario() {
+    public String registrarUsuario(@ModelAttribute("usuario") Usuario usuario, Model model,
+                                   RedirectAttributes attr) {
 
+        if (usuario.getIdusuario() == 0){
+            attr.addFlashAttribute("msg1", "Usuario creado exitosamente");
+            usuarioRepository.save(usuario);
+            return "redirect:/producto";
+        }else {
+            attr.addFlashAttribute("msg1", "Usuario actualizado exitosamente");
+            usuarioRepository.save(usuario);
+            return "redirect:/producto";
+        }
 
-
-        return "algunavista";
     }
 
 @Autowired
@@ -60,6 +79,18 @@ public class UsuarioController {
     }
 
 
+    @GetMapping("/borrar")
+    public String borrar (@RequestParam("idusuario") int id, RedirectAttributes attr ){
+
+        Optional<Usuario> borrado = usuarioRepository.findById(id);
+
+        if (borrado.isPresent()){
+            usuarioRepository.deleteById(id);
+            attr.addFlashAttribute("msg1", "Borrado exitosamente");
+        }
+
+        return "producto/listar";
+    }
 
 
 
